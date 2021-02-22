54452e502c5f44148176349690eaad8a323431ef
Hassan Chehouri <hassanchehouri2000@hotmail.com>
2021-02-22 18:06:56 -0500

    test2

--- src/composants/Entete.jsx
+++ src/composants/Entete.jsx
@@ -5,7 +5,7 @@ export default function Entete() {
   return (
     <header className="Entete">
       <div className="logo">Signets</div>
-      <div className="avatar"> <a>Thomas Lorenzo</a> <img src={avatar} className="IMG" alt="Thomas Lorenzo" /></div>
+      <div className="avatar"> <a>Hassan Chehouri</a> <img src={avatar} className="IMG" alt="Thomas Lorenzo" /></div>
     </header>
   );
   
